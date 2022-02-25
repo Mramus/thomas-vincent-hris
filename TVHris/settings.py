bc090483c474ae1c816c7f7ecb977908dfcabf1d
danabernice <bernicecunanan@gmail.com>
2022-02-25 14:21:52 +0800

    connected mysql

--- TVHris/settings.py
+++ TVHris/settings.py
@@ -75,8 +75,11 @@ WSGI_APPLICATION = 'TVHris.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'tvchrm',
+        'USER' : 'test',
+        'PASSWORD' : 'root',
+        'HOST' : 'localhost',
     }
 }
 
