7faed25fd43727dc7629c83058c12e69089f3c04
Thomas A.V.T.E. Testa <thomas.testa@obf.ateneo.edu>
2022-02-28 22:23:38 +0800

    added project pages

--- hris/urls.py
+++ hris/urls.py
@@ -3,5 +3,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.demo, name='demo')
+    path('', views.demo, name='demo'),
+    path('projects', views.projects, name='projects'),
+    path('add_project', views.add_project, name='add_project'),
 ]
--- hris/views.py
+++ hris/views.py
@@ -1,4 +1,14 @@
 from django.shortcuts import render
+from django.contrib import messages
+from .models import UserT, WorkerT, ProjectT, AssignmentT, EvaluationReportT
 
 def demo(request):
     return render(request, 'hris/demo.html')
+
+def projects(request):
+    project_objects = ProjectT.objects.all()
+    return render(request, 'hris/projects.html', {'projects':project_objects})
+
+def add_project(request):
+    #code here
+    return render(request, 'hris/add_project.html')
