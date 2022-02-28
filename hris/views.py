43273169c40d459e913c7d61aa2ba6ec6328f4b7
Thomas A.V.T.E. Testa <thomas.testa@obf.ateneo.edu>
2022-02-28 23:06:51 +0800

    added create project functionality

--- hris/views.py
+++ hris/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.contrib import messages
 from .models import UserT, WorkerT, ProjectT, AssignmentT, EvaluationReportT
 
@@ -10,5 +10,17 @@ def projects(request):
     return render(request, 'hris/projects.html', {'projects':project_objects})
 
 def add_project(request):
-    #code here
-    return render(request, 'hris/add_project.html')
+    if(request.method=="POST"):
+        ptitle = request.POST.get('ptitle')
+        ptype = request.POST.get('ptype')
+        plocation = request.POST.get('plocation')
+        pclient = request.POST.get('pclient')
+        pclientcontact = request.POST.get('pclientcontact')
+        ppic = request.POST.get('ppic')
+        ppiccontact = request.POST.get('ppiccontact')
+        pstartdate = request.POST.get('pstartdate')
+        penddate = request.POST.get('penddate')        
+        ProjectT.objects.create(project_title=ptitle, project_type=ptype, project_location=plocation, client=pclient, client_contact_number=pclientcontact, project_in_charge=ppic, project_in_charge_contact_number=ppiccontact, start_date=pstartdate, end_date=penddate)
+        return redirect('projects')
+    else:
+        return render(request, 'hris/add_project.html')
